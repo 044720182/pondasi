8a3900f7df3465a7bfd99173634ea375dfc77ea4
Yusuf Ibr <devway@bk.ru>
2022-12-21 16:11:29 +0700

    first commit

---
+++ src/main/java/com/yoesoff/pondasi/PondasiApplication.java
@@ -0,0 +1,13 @@
+package com.yoesoff.pondasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PondasiApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PondasiApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/yoesoff/pondasi/PondasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoesoff.pondasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondasiApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
